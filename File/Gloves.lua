fc2951eed6d6361d42a0ffaa5935e02fa852e148
Giangplay <123900060+Giangplay@users.noreply.github.com>
2023-08-19 06:54:54 +0700

    Update Gloves.lua

--- File/Gloves.lua
+++ File/Gloves.lua
@@ -22,7 +22,7 @@
     ["Glitch"] = game.ReplicatedStorage.GeneralHit,
     ["Snowball"] = game.ReplicatedStorage.GeneralHit,
     ["fish"] = game.ReplicatedStorage.GeneralHit,
-    ["🗿"] = game.ReplicatedStorage.GeneralHit,
+    [""] = game.ReplicatedStorage.GeneralHit,
     ["Obby"] = game.ReplicatedStorage.GeneralHit,
     ["Voodoo"] = game.ReplicatedStorage.GeneralHit,
     ["Leash"] = game.ReplicatedStorage.GeneralHit,
@@ -115,6 +115,7 @@
     ["Thor"] = game.ReplicatedStorage.ThorHit,
     ["Retro"] = game.ReplicatedStorage.RetroHit,
     ["Cloud"] = game.ReplicatedStorage.CloudHit,
+    ["Null"] = game.ReplicatedStorage.NullHit
     ["Custom"] = game.ReplicatedStorage.CustomHit,
     
     ----------------------------------------
