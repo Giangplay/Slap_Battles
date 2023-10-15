b38543111b85ef0fe2ba28319953beeffbd25505
Playgiang <123900060+Giangplay@users.noreply.github.com>
2023-10-15 07:09:25 +0700

    Update AutoFarm Slapple.lua

--- File/AutoFarm Slapple.lua
+++ File/AutoFarm Slapple.lua
@@ -1,30 +1,29 @@
-if not game:IsLoaded() then 
-     game.Loaded:Wait() 
- end 
-if game.PlaceId == 6403373529 or game.PlaceId == 9015014224 or game.PlaceId == 11520107397 then 
-if
- repeat task.wait() until game.Players.LocalPlayer.Character ~= nil and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") 
- if game.Players.LocalPlayer.Character:FindFirstChild("entered") == nil then 
-     repeat task.wait() 
-         firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, workspace.Lobby.Teleport1, 0) 
-         firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, workspace.Lobby.Teleport1, 1) 
-     until game.Players.LocalPlayer.Character:FindFirstChild("entered") 
- end 
- for i, v in ipairs(workspace.Arena.island5.Slapples:GetDescendants()) do 
-                 if v.Name == "Glove" and v:FindFirstChildWhichIsA("TouchTransmitter") then 
-                     firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, v, 0) 
-         firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, v, 1) 
-                 end 
-             end 
- local serverList = {} 
- for _, v in ipairs(game:GetService("HttpService"):JSONDecode(game:HttpGetAsync("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100")).data) do 
-         if v.playing and type(v) == "table" and v.maxPlayers > v.playing and v.id ~= game.JobId then 
-                 serverList[#serverList + 1] = v.id 
-         end 
- end 
- if #serverList > 0 then 
-         game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, serverList[math.random(1, #serverList)]) 
- else 
-     error("No servers found") 
- end
- end
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+if game.PlaceId == 6403373529 or game.PlaceId == 9015014224 or game.PlaceId == 11520107397 then
+repeat task.wait() until game.Players.LocalPlayer.Character ~= nil and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
+if game.Players.LocalPlayer.Character:FindFirstChild("entered") == nil then
+    repeat task.wait()
+        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, workspace.Lobby.Teleport1, 0)
+        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, workspace.Lobby.Teleport1, 1)
+    until game.Players.LocalPlayer.Character:FindFirstChild("entered")
+end
+task.wait(0.11)
+for i, v in ipairs(workspace.Arena.island5.Slapples:GetDescendants()) do
+                if v.Name == "Glove" and v:FindFirstChildWhichIsA("TouchTransmitter") then
+                    firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, v, 0)
+                    firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, v, 1)
+                end
+            end
+local serverList = {}
+for _, v in ipairs(game:GetService("HttpService"):JSONDecode(game:HttpGetAsync("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100")).data) do
+	if v.playing and type(v) == "table" and v.maxPlayers > v.playing and v.id ~= game.JobId then
+		serverList[#serverList + 1] = v.id
+	end
+end
+if #serverList > 0 then
+	game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, serverList[math.random(1, #serverList)])
+end
+end
