84184469dcf5d9835ed903e430c8b1f8d5ef8408
Playgiang <123900060+Giangplay@users.noreply.github.com>
2023-11-24 20:13:27 +0700

    Update Slap_Battles.lua

--- Slap_Battles.lua
+++ Slap_Battles.lua
@@ -105,7 +105,7 @@ screenGui:Destroy()
 local OrionLib = loadstring(game:HttpGet(("https://raw.githubusercontent.com/Giangplay/Script/main/Orion_Library_PE_V2.lua")))()
 local GameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
 if game.PlaceId == 6403373529 or game.PlaceId == 9015014224 or game.PlaceId == 11520107397 then
-local Window = OrionLib:MakeWindow({IntroText = "Slap Battles 👏", Name = ("Slap Battles 👏".." | ".. identifyexecutor()), HidePremium = false, SaveConfig = true, IntroEnabled = true, ConfigFolder = "slap battles"})
+local Window = OrionLib:MakeWindow({IntroText = "Slap Battles 👏", IntroIcon = "rbxassetid://7733954058",Name = ("Slap Battles 👏".." | ".. identifyexecutor()), HidePremium = false, SaveConfig = true, IntroEnabled = true, ConfigFolder = "slap battles"})
 
 ---Bypass---
 
