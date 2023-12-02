89012321596e5d03e756879294364c5c0b73c649
Playgiang <123900060+Giangplay@users.noreply.github.com>
2023-12-02 19:35:16 +0700

    Update AutoFarm Slapple.lua

--- File/AutoFarm Slapple.lua
+++ File/AutoFarm Slapple.lua
@@ -3,20 +3,27 @@ if not game:IsLoaded() then
 end
 
 if game.PlaceId == 6403373529 or game.PlaceId == 9015014224 or game.PlaceId == 11520107397 then
-repeat task.wait() until game.Players.LocalPlayer.Character ~= nil and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
+local teleportFunc = queueonteleport or queue_on_teleport or syn and syn.queue_on_teleport
+if teleportFunc then
+    teleportFunc([[
+repeat task.wait() until game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
 if game.Players.LocalPlayer.Character:FindFirstChild("entered") == nil then
     repeat task.wait()
         firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, workspace.Lobby.Teleport1, 0)
         firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, workspace.Lobby.Teleport1, 1)
     until game.Players.LocalPlayer.Character:FindFirstChild("entered")
 end
-task.wait(0.11)
+task.wait(0.6)
 for i, v in ipairs(workspace.Arena.island5.Slapples:GetDescendants()) do
                 if v.Name == "Glove" and v:FindFirstChildWhichIsA("TouchTransmitter") then
                     firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, v, 0)
                     firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, v, 1)
                 end
             end
+task.wait(0.6)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Giangplay/Slap-Battles-and-Ability-wars/main/File/AutoFarm%20Slapple.lua"))()
+            ]])
+end
 local serverList = {}
 for _, v in ipairs(game:GetService("HttpService"):JSONDecode(game:HttpGetAsync("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100")).data) do
 	if v.playing and type(v) == "table" and v.maxPlayers > v.playing and v.id ~= game.JobId then
