5a4711a417324250f7b421620c04b657d4b84077
Playgiang <123900060+Giangplay@users.noreply.github.com>
2024-06-20 17:16:59 +0700

    Fix script not work 😭

--- File/Farm Bob.lua
+++ File/Farm Bob.lua
@@ -6,24 +6,18 @@ if game.PlaceId == 6403373529 then
 local teleportFunc = queueonteleport or queue_on_teleport or syn and syn.queue_on_teleport
 if teleportFunc then
     teleportFunc([[
-repeat task.wait() until game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
-wait(0.25)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Giangplay/Slap_Battles/main/File/Farm%20Bob.lua"))()
+repeat task.wait() until game.Players.LocalPlayer
 wait(0.25)
 if not game:GetService("BadgeService"):UserHasBadgeAsync(game.Players.LocalPlayer.UserId, 2125950512) then
 fireclickdetector(workspace.Lobby.Replica.ClickDetector)
 wait(0.25)
-repeat task.wait() until game.Players.LocalPlayer.Character
-if not game.Players.LocalPlayer.Character:FindFirstChild("entered") and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
-repeat task.wait()
 firetouchinterest(game.Players.LocalPlayer.Character:WaitForChild("Head"), workspace.Lobby.Teleport1.TouchInterest.Parent, 0)
 firetouchinterest(game.Players.LocalPlayer.Character:WaitForChild("Head"), workspace.Lobby.Teleport1.TouchInterest.Parent, 1)
-until game.Players.LocalPlayer.Character:FindFirstChild("entered")
-end
 wait(0.4)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Giangplay/Slap_Battles/main/File/Farm%20Bob.lua"))()
+wait(0.2)
 for i = 1, 2500 do
 game:GetService("ReplicatedStorage").Duplicate:FireServer(true)
-end
 end
 end
             ]])
