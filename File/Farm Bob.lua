f5f8d3881cd49118e3b1150d1f8f9ed568db2476
Playgiang <123900060+Giangplay@users.noreply.github.com>
2024-06-21 08:44:13 +0700

    Update Farm Bob.lua

--- File/Farm Bob.lua
+++ File/Farm Bob.lua
@@ -1,13 +1,16 @@
-if not game:IsLoaded() then
-    game.Loaded:Wait()
-end
-
 if game.PlaceId == 6403373529 then
 local teleportFunc = queueonteleport or queue_on_teleport or syn and syn.queue_on_teleport
 if teleportFunc then
     teleportFunc([[
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
 repeat task.wait() until game.Players.LocalPlayer
 wait(0.25)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Giangplay/Slap_Battles/main/File/Farm%20Bob.lua"))()
+            ]])
+end
+
 if not game:GetService("BadgeService"):UserHasBadgeAsync(game.Players.LocalPlayer.UserId, 2125950512) then
 fireclickdetector(workspace.Lobby.Replica.ClickDetector)
 wait(0.25)
@@ -17,10 +20,7 @@ wait(0.4)
 for i = 1, 2500 do
 game:GetService("ReplicatedStorage").Duplicate:FireServer(true)
 end
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Giangplay/Slap_Battles/main/File/Farm%20Bob.lua"))()
-end
-            ]])
-end
+wait(0.4)
 local serverList = {}
 for _, v in ipairs(game:GetService("HttpService"):JSONDecode(game:HttpGetAsync("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100")).data) do
 	if v.playing and type(v) == "table" and v.maxPlayers > v.playing and v.id ~= game.JobId then
@@ -29,5 +29,6 @@ for _, v in ipairs(game:GetService("HttpService"):JSONDecode(game:HttpGetAsync("
 end
 if #serverList > 0 then
 	game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, serverList[math.random(1, #serverList)])
+end
 end
 end
